3c068d9ee26c052a6418fc8c7565a05058758e6d
wjhuang2016 <huangwenjun1997@gmail.com>
2020-08-04 11:18:43 +0800

    12
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- store/tikv/client_batch.go
+++ store/tikv/client_batch.go
@@ -625,6 +625,7 @@ func sendBatchRequest(ctx context.Context, addr string, batchConn *batchConn, re
 		if span := opentracing.SpanFromContext(ctx); span != nil && span.Tracer() != nil {
 			span1.Finish()
 		}
+		t := time.Since(start).Seconds()
 		stmtExec := ctx.Value(execdetails.StmtExecDetailKey)
 		if stmtExec != nil {
 			detail := stmtExec.(*execdetails.StmtExecDetails)
@@ -643,7 +644,7 @@ func sendBatchRequest(ctx context.Context, addr string, batchConn *batchConn, re
 			sendReqHistCache.Store(key, v)
 		}
 
-		v.(prometheus.Observer).Observe(time.Since(start).Seconds())
+		v.(prometheus.Observer).Observe(t)
 		return tikvrpc.FromBatchCommandsResponse(res)
 	case <-ctx.Done():
 		atomic.StoreInt32(&entry.canceled, 1)
